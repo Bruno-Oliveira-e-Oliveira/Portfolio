06c978b27ec89c6ce8257e25c766cb0188c1af74
BrunoOliveira <brunooliveiraeoliveira@outlook.com>
2022-05-07 12:02:47 -0300

    Fixed the layout and the bug of mobile menu, finishined the changes of content

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,6 @@
 function show_menu() {
-    let menu = document.getElementById("menu");
-    let menucheck = document.getElementById("menu-check");
+    let menu = document.getElementById('menu');
+    let menucheck = document.getElementById('menu-check');
     let checked = menucheck.checked;
     if (checked) {
         menu.style.display = 'block';
@@ -11,12 +11,20 @@ function show_menu() {
 
 function remove_menu() {
     if (screen.width < 1200) {
-        let menu = document.getElementById("menu");
-        let menucheck = document.getElementById("menu-check");
+        let menu = document.getElementById('menu');
+        let menucheck = document.getElementById('menu-check');
         menu.style.display = 'none';
         menucheck.checked = false;    
     } else {
-        let menu = document.getElementById("menu");
+        let menu = document.getElementById('menu');
         menu.style.display = 'block';
     }
+}
+
+window.onresize = () => {
+    if (window.screen.availWidth >= 1200){
+        let menucheck = document.getElementById('menu-check');
+        menucheck.checked = true;
+        show_menu();
+    };
 }
