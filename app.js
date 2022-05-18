d64644b2d8c7bd7a895abd3f20a5e2422b0cd31d
BrunoOliveira <brunooliveiraeoliveira@outlook.com>
2022-05-18 18:34:23 -0300

    Error handler routine

--- app.js
+++ app.js
@@ -9,7 +9,33 @@ const port = enviromentObject.port;
 
 app.use(express.static('public'));
 
-app.get('', (req, res) => {
+app.get('', (req, res, next) => {
+    console.log('Access done at ' + getDateNow())
+    res.sendFile('index.html', {"root": __dirname});
+});
+
+// ***************************************************** REMOVER
+app.get('/500', (req, res, next) => {
+    throw new Error('teste deu erro');
+});
+
+app.use((req, res, next) => {
+    console.log('Page not found. (' + getDateNow() +')');
+    res.status(404).send('Página não encontrada');
+});
+
+app.use((err, req, res, next) => {
+    console.log('An unhandled exception occurred. (' + getDateNow() +')');
+    res.sendStatus(500);
+});
+
+app.listen(port, () =>{
+    console.log('------------------------------------------------------------');
+    console.log(`Listening on port ${port}`)
+    console.log('------------------------------------------------------------');
+});
+
+function getDateNow() {
     let ts = Date.now();
     let date = new Date(ts);
     let day = date.getDate();
@@ -18,11 +44,7 @@ app.get('', (req, res) => {
     let hour = date.getHours();
     let minutes = date.getMinutes();
     let seconds = date.getSeconds();
+    let dateNow = year + '-' + month + '-' + day + ' ' + hour + ':' + minutes + ':' + seconds;
 
-    console.log('Access done at ' + year + '-' + month + '-' + day + ' ' + hour + ':' + minutes + ':' + seconds)
-    res.sendFile('index.html', {"root": __dirname});
-});
-
-app.listen(port, () =>{
-    console.log(`Listening on port ${port}`);
-});
+    return dateNow;
+}
